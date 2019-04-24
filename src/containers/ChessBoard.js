44a78eeb3472cdd58aeaf893deeed6e31e5d803c
Ramesh Ravi <ramesh7128@gmail.com>
2019-04-24 12:26:27 +0530

    edited css styling for back button

--- src/containers/ChessBoard.js
+++ src/containers/ChessBoard.js
@@ -2,45 +2,12 @@ import React, { Component } from 'react';
 // import { Chessground } from 'chessground';
 import { Icon, Button } from 'semantic-ui-react';
 import Chessground from 'react-chessground';
+import Fullscreen from "react-full-screen";
 
 class ChessBoard extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            width: window.innerWidth - 100,
-            height: '',
-            fullScreen: false
-        }
         this.handleFullScreen = this.handleFullScreen.bind(this);
-    }
-
-    // updateDimensions() {
-    //     let update_width = window.innerWidth - 100;
-    //     let update_height = Math.round(update_width / 4.4);
-    //     this.setState({ width: update_width, height: update_height });
-
-    // }
-
-    // componentDidMount() {
-    //     this.updateDimensions();
-    //     window.addEventListener("resize", this.updateDimensions.bind(this));
-    // }
-
-    // componentDidUpdate(prevProps, prevState) {
-    //     console.log(this.state.width, prevState.width);
-    //     if (((this.state.width - prevState.width) > 5) || ((prevState.width - this.state.width) > 5)) {
-    //         window.location.reload();
-    //     }
-
-    // }
-
-    toggleFullScreen(event) {
-        console.log(event.target);
-        if (event.key == 'Enter') {
-            this.setState(state => ({
-                fullScreen: !state.fullScreen,
-            }))
-        }
     }
 
     handleFullScreen() {
@@ -63,14 +30,8 @@ class ChessBoard extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className='fullscreen-button'>
-                    {!this.state.fullScreen &&
-                        <Button onClick={this.handleFullScreen} color='orange'>FullScreen</Button>
-                    }
-                </div>
-
                 <div id='chess-board-section' onKeyDown={this.toggleFullScreen}>
-                    <div onClick={this.props.toggleBoardActive}>
+                    <div onClick={this.props.toggleBoardActive} className='board-back-button'>
                         <Icon name='arrow left' />Back
                     </div>
                     <Chessground resizable={true} fen={this.props.fen} coordinates={false} />
